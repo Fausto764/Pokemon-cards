fc0d3de0def61e4f0dbcef4c6f1c63e0fdeb8b24
fast <faustogarcia1357@gmail.com>
2023-01-08 22:50:40 -0300

    Creacion de archivos terminada

---
+++ src/components/Cards/Cards.jsx
@@ -0,0 +1,11 @@
+import './Cards.css'
+
+export function Cards (){
+    return(
+        <main className="main">
+           <div className="main__cards">Card1</div>
+           <div className="main__cards">Card2</div>
+           <div className="main__cards">Card3</div>
+        </main>
+    )
+}
---
+++ src/components/Peticion/Peticion.jsx
@@ -0,0 +1,5 @@
+export function Peticion(){
+    return(
+        <h1>info</h1>
+    )
+}
